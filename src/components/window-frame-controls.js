b8555d01ce65340d195e43d0cb9386aff544b611
Philip Jackson <p-jackson@live.com>
2015-08-25 23:22:14 +1200

    Fixed the WindowFrameControls build

--- src/components/window-frame-controls.js
+++ src/components/window-frame-controls.js
@@ -3,15 +3,15 @@ import remote from 'remote';
 import Button from 'components/button';
 
 const styles = {
-  minimize = { marginRight: 1 },
-  minimizeIcon = { position: 'relative', top: 2 },
-  closeHover = { background: '#E81123', color: '#fff' }
+  minimize: { marginRight: 1 },
+  minimizeIcon: { position: 'relative', top: 2 },
+  closeHover: { background: '#E81123', color: '#fff' }
 }
 
 class WindowFrameControls extends React.Component {
   render() {
     return (
-      <div style={...this.props}>
+      <div {...this.props}>
         <Button style={styles.minimize} onClick={this.onMinimize}>
           <span style={styles.minimizeIcon} className="icon-minus"></span>
         </Button>
